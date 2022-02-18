6ddcb0469e442e8642fbb486b8ab915100a88557
PrashantBtkl <prashant44backup@gmail.com>
2022-02-18 11:50:00 +0530

    add integration test code

---
+++ it-test.py
@@ -0,0 +1,103 @@
+import requests
+
+CARTS_URL = "http://carts/carts"
+CUSTOMERS_URL = "http://user/customers"
+
+
+# create users
+def create_user(username):
+	user_created = requests.models.Response
+	try:
+		user_created = requests.post(
+			CUSTOMERS_URL,
+			json = {"username": username,"password":"123","email":"1@mail.com","firstName":"foo","lastName":"bar"}
+			)
+	except Exception as e:
+		print("Failed to create user")
+		raise e
+
+	return user_created.json()["id"]
+
+
+# get id
+def get_customer(user_id):
+	customer = requests.models.Response
+	try:
+		customer = requests.get(CUSTOMERS_URL+"/"+user_id)
+	except Exception as e:
+		print("Failed to get customer")
+		delete_customer(user_id)
+		raise e
+	return customer.json()["username"]
+
+#TEST CARTS
+
+# add item to cart for previous user
+def add_item_to_cart(item_id, quantity, user_id):
+	cart = requests.models.Response
+	try:
+		cart = requests.post(
+			CARTS_URL+"/"+user_id+"/items",
+			json = {"itemId":item_id, "quantity":quantity, "unitPrice":15.0}
+			)
+	except Exception as e:
+		print("Failed to add item to cart")
+		delete_customer(user_id)
+		raise e
+
+	return cart.json()
+
+
+###CLEANUP### 
+def delete_customer(user_id):
+	try:
+		cust = requests.delete(CUSTOMERS_URL+"/"+user_id)
+	except Exception as e:
+		print("Failed to delete customer")
+		raise e
+	print("Successfully deleted customer")
+
+def delete_cart(user_id):
+	try:
+		carts = requests.delete(CARTS_URL+"/"+user_id)
+	except Exception as e:
+		print("Failed to delete cart")
+		raise e
+	print("Successfully deleted cart")
+###CLEANUP###
+
+
+def main():
+	username = "foobar1"
+	item_id = "510a0d7e-8e83-4193-b483-e27e09ddc34d"
+	quantity = 1
+
+	#TEST USER SERVICE
+	test_user_id = create_user(username)
+	test_username = get_customer(test_user_id)
+	try:
+		assert test_username, username
+		print("Success test : username match")
+	except AssertionError:
+		print("Failed test : username match")
+
+	#TEST CART SERVICE
+	cart = add_item_to_cart(item_id, quantity, test_user_id)
+	try:
+		assert cart["itemId"], item_id
+		print("Success test : cart item_id matched")
+		assert cart["quantity"], quantity
+		print("Success test : cart item quantity matched")
+	except AssertionError:
+		print("Failed test : adding item to cart")
+
+
+	print("----- all test cases passed --------")
+	print("cleanup starting")
+	#CLEANUP
+	delete_customer(test_user_id)
+	delete_cart(test_user_id)
+
+
+if __name__ == "__main__":
+	main()
